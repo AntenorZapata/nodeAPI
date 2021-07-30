9b224d81de45490595a0c95693b29d81ea180d5f
Antenor <antenorzapata@gmail.com>
2021-07-29 22:50:12 -0300

    config: install mongoose and connect mongo db

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const db = process.env.DATABASE;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('db connection success');
+  });
 
 const port = process.env.PORT;
 
