4031758407fa4659ce491f77ee3014dfde8f93cf
Antenor <antenorzapata@gmail.com>
2021-08-03 20:11:34 -0300

    refactoring: create error module

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 
 const guitarRouter = require('./routes/guitarRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalError = require('./controllers/errorController');
 
 // Middlewares
 
@@ -24,26 +26,10 @@ app.use('/api/v1/guitars', guitarRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 //Global error handler
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalError);
 
 module.exports = app;
