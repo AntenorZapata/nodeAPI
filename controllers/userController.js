24de1803643c1ce8e57ad2929a8c9338a1513ed3
Antenor <antenorzapata@gmail.com>
2021-08-09 03:54:47 -0300

    feature: update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -17,6 +26,31 @@ const createUser = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: 'Route dosent exists',
+  });
+};
+
+const updateMe = async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser,
+    },
   });
 };
 
@@ -47,4 +81,5 @@ module.exports = {
   getUser,
   upDateUser,
   deleteUser,
+  updateMe,
 };
